d042a08b7b8badf11fc49da8eac45c4ddcce870a
Alejandro Arguinzones <arguinzones.alejandro@gmail.com>
2023-06-02 10:01:45 -0400

    showing puppy info after click

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,33 @@ import { puppyList } from "./data.js";
 import { useState } from "react";
 function App() {
   const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
   console.log("puppyList: ", puppies);
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
+  console.log(featuredPup);
   return (
     <div>
+      {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
+
       {puppies.map((puppy) => {
-        return <p key={puppy.id}>{puppy.name}</p>;
+        return (
+          <p
+            onClick={() => {
+              setFeatPupId(puppy.id);
+            }}
+            key={puppy.id}
+          >
+            {puppy.name}
+          </p>
+        );
       })}
     </div>
   );
